6fc34da4198f7a1d9ca82823a130da6df46f655a
ohailstorm <ohailstorm@users.noreply.github.com>
2020-02-26 16:34:55 +0100

    change to new site

---
+++ precache-manifest.1402d72db861cd50c852eba71d7877dc.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "42e22b71004b0bb506fb6e3fc00ff2ba",
+    "url": "/index.html"
+  },
+  {
+    "revision": "8cb2770d6f113681dcd8",
+    "url": "/static/css/main.25249b2f.chunk.css"
+  },
+  {
+    "revision": "f41f28de2cbc76748d65",
+    "url": "/static/js/2.0673b141.chunk.js"
+  },
+  {
+    "revision": "2ee391ed19a97911d1a7197f70edf108",
+    "url": "/static/js/2.0673b141.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "8cb2770d6f113681dcd8",
+    "url": "/static/js/main.d3856615.chunk.js"
+  },
+  {
+    "revision": "0b5f5ca209b0457ac514",
+    "url": "/static/js/runtime-main.29742585.js"
+  }
+]);
